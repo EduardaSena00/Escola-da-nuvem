a08a5c2509e807d02aaf195890bc026ddb87e107
Eduardasena <me824005@gmail.com>
2025-05-25 10:16:18 -0300

    Meu primeiro commit

---
+++ Ativdade_01/calcula_diferenca.py
@@ -0,0 +1,13 @@
+# Esse código tem como objetivo ler valores inteiros inputados pelo usuário
+
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+# Calculo da diferença
+DIFERENCA = (A * B) - (C * D)
+
+# Exibir resultado
+print("A fórmula para a difernça é: (A * B) - (C * D)")
+print(f"O resultado da diferença é:  {DIFERENCA}")
---
+++ Atividade_02/converter_reais.py
@@ -0,0 +1,4 @@
+valor_em_reais = 100
+taxa_do_dolar = 5
+taxa_do_euro = 6.15
+
